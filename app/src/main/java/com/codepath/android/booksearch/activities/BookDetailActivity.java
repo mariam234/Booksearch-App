4d68955f7762517572a394c5d41bbd4ca0944971
mariam234 <mariamdiallo@fb.com>
2018-07-05 11:19:53 -0700

    first commit

---
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -0,0 +1,72 @@
+package com.codepath.android.booksearch.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.android.booksearch.GlideApp;
+import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.models.Book;
+
+import org.parceler.Parcels;
+
+public class BookDetailActivity extends AppCompatActivity {
+    private ImageView ivBookCover;
+    private TextView tvTitle;
+    private TextView tvAuthor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_detail);
+
+        // Fetch views
+        ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
+
+        // Extract book object from intent extras
+        Book book = (Book) Parcels.unwrap(getIntent().getParcelableExtra("book"));
+
+        // change action bar title to be book title
+        getSupportActionBar().setTitle(book.getTitle());
+
+        // Use book object to populate data into views
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+        GlideApp.with(this)
+                .load(book.getCoverUrl())
+                .into(ivBookCover);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSubmit(View v){
+        this.finish();
+    }
+}
